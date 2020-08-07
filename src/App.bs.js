b838c9d1461e8523323006a87a43dbd5cdd08aad
Zain Fathoni <zain.fathoni@gmail.com>
2020-08-07 23:08:45 +0800

    Track JS files

---
+++ src/App.bs.js
@@ -0,0 +1,46 @@
+
+
+import * as Curry from "bs-platform/lib/es6/curry.js";
+import * as React from "react";
+import * as LogoSvg from "./logo.svg";
+
+import './App.css'
+;
+
+var logo = LogoSvg.default;
+
+function App(Props) {
+  var match = React.useState((function () {
+          return 0;
+        }));
+  var setCount = match[1];
+  return React.createElement("div", {
+              className: "App"
+            }, React.createElement("header", {
+                  className: "App-header"
+                }, React.createElement("img", {
+                      className: "App-logo",
+                      alt: "logo",
+                      src: logo
+                    }), React.createElement("p", undefined, "Hello Vite + Reason React"), React.createElement("p", undefined, React.createElement("button", {
+                          onClick: (function (param) {
+                              return Curry._1(setCount, (function (count) {
+                                            return count + 1 | 0;
+                                          }));
+                            })
+                        }, "count is: " + String(match[0]))), React.createElement("p", undefined, "Edit ", React.createElement("code", undefined, "App.jsx "), "and save to test HMR updates."), React.createElement("a", {
+                      className: "App-link",
+                      href: "https://reasonml.github.io/reason-react/",
+                      rel: "noopener noreferrer",
+                      target: "_blank"
+                    }, "Learn Reason React")));
+}
+
+var make = App;
+
+export {
+  logo ,
+  make ,
+  
+}
+/*  Not a pure module */
---
+++ src/Index.bs.js
@@ -0,0 +1,15 @@
+
+
+import * as React from "react";
+import * as ReactDOMRe from "reason-react/src/ReactDOMRe.js";
+import * as App$ViteReasonReactStarter from "./App.bs.js";
+
+import './index.css'
+;
+
+ReactDOMRe.renderToElementWithId(React.createElement(App$ViteReasonReactStarter.make, { }), "root");
+
+export {
+  
+}
+/*  Not a pure module */
